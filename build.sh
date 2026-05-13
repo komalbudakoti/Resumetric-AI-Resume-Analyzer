0c7757cadb65c374e6f2187e649eacc62d2ae5f1
komalbudakoti <komalbudakoti22@gmail.com>
2026-05-13 20:59:21 +0530

    fix: streamlined build script for Render environment

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install system build dependencies required for packages like blis, thinc, and spacy
-apt-get update && apt-get install -y build-essential python3-dev libblas-dev liblapack-dev
-
 # Upgrade packaging tools to avoid wheel/build errors
 python3 -m pip install --upgrade pip setuptools wheel
 
